6f52fe80dfbfa0abd325ceb0cef24681b27c689d
Zach Bray <zachdbray@gmail.com>
2023-08-16 22:20:24 -0400

    some basic puts and variables

--- giraffe.rb
+++ giraffe.rb
@@ -1,5 +1,50 @@
 puts "Hello World"
 puts "Hello World"
 puts "Hello World"
+puts "Hello World(puts end)"
+puts "----"
+print "(print start)Hello World"
+print "Hello World"
+print "Hello World"
+print "Hello World(print end)"
+puts "(puts start)Hello World"
 puts "Hello World"
+puts "Hello World"
+puts "Hello World"
+# Output
+# puts will print the input and then create a line break
+# print will print the input and not create a line break
+
+puts "There once was a man named George"
+puts "he was 70"
+puts "he liked the name George "
+puts "didn't like being 70"
+puts "----"
+# Variables
+
+# In order to change pieces of data here like "George" or "70" you had to modify each value by sight 
+
+# You can use variables to replace 35 and George with containers that can be accessed all at once
+
+# variables should be in lower case and use snake_case
+
+character_name = "Mike"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age)
+puts ("he liked the name " + character_name)
+puts ("didn't like being " + character_age)
+
+# anytime you're using a string of text with variable name, 
+# you need to surround the entire input with parenthesis
+
+# you can also change the value of a variable after it's been called
+
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age)
+character_name = "Zach"
+puts ("he liked the name " + character_name)
+puts ("didn't like being " + character_age)
+
+# here we added a new character_name def line changing the name to Zach and it delivered for the next time character_name was called
 
--- notes.md
+++ notes.md
@@ -1,6 +1,6 @@
 ## Ruby Basics
 
-How to print
+How to print something
 
 ```ruby
 print "Griaffe Academy"
@@ -14,10 +14,20 @@ puts "Zach"
 
 will print Zach right after
 
-puts will print the input and then create a line break
+puts will print something and then add a new line
+print will print something on a new line
 
-print will print the input and not create a line break
 
 ## How is Ruby executed?
 
 Ruby treats every line as a separate instruction and executes instructions in order
+
+## Variables
+
+In Ruby you're going to deal with a lot of data. Ruby makes it easier to deal with lots of data using the container "variable"
+
+Pieces of data can be stored in variables and variables can refered to reference, alter, use, etc.
+
+Variable names must be lower case and snake_case if they contain multiple words
+
+lines of code that contain text and variables must be placed inside parentheses 
